2b3ae0607c88913a0b9ad18ec11fe67b8258bc1e
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-07-07 19:55:28 -0600

    initial commit

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+bases = ["first", "second", "third", "home"]
+
+odd_numbers = [3, 5, 7, 9]
+
+flohwts = [2.4, 3.6, 7.5, "root beer", 2.9938454543, "<-- definitely not a debit card number"]
+
+boolean_values = [true, false, false, true]
+
