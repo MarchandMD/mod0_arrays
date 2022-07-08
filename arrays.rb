226a262f5707a7d66f612e52855a7f0cbc6ca1f9
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-07-07 19:59:57 -0600

    apply methods to arrays

--- arrays.rb
+++ arrays.rb
@@ -6,3 +6,14 @@ flohwts = [2.4, 3.6, 7.5, "root beer", 2.9938454543, "<-- definitely not a debit
 
 boolean_values = [true, false, false, true]
 
+# Returns the count of elements in the bases array.
+bases.length
+
+# Removes and returns trailing elements.
+odd_numbers.pop
+
+# Removes and returns leading elements.
+flohwts.shift
+
+# will count the values using cardinal numbers
+boolean_values.count
